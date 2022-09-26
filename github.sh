9c69f62cc58b41a0f4952feb76a7a649d67dfa6f
Dinh Nguyen <dinhnt@senspark.com>
2022-09-27 00:02:59 +0700

    Update done
    Signed-off-by: Dinh Nguyen <dinhnt@senspark.com>

--- github.sh
+++ github.sh
@@ -12,17 +12,7 @@ Requires github environment variables (additional may be required for specific c
 Available commands:  
     build <project_name>    start build of given project
                             outputs build request id
-                            requires: GITHUB_REF
-    status <build_number>   get status of build identified by given build number
-                            outputs one of: success | failed | null
-    kill <build_number>     kills running build identified by given build number                            
-    hash <position>         get revision hash on given positions
-                            available positions:
-                                last        hash of last succesfull build commit
-                                            only commits of 'build' job are considered
-                                            accepts: GITHUB_REF, if ommited no branch filtering
-                                current     hash of current commit
-                                            requires: GITHUB_SHA                         
+                            requires: GITHUB_REF                        
     help                    display this usage text                             
 EOM
 
@@ -127,122 +117,33 @@ function get_branch {
 # Trigger build in github
 #
 # Build in github is triggered by dispatching custom event with job parameter set to project name.
-# After event is triggered zero, one or more workflows can be started.
-# We need to check all started workflows if they contain job with same name as project name.
-# If such a job is found then it's workflow's run id is returned as build id.
-#
-# There is timeout set to 5 seconds for the workflow run to be started.
-# If timeout is reached it is considered that job was not defined and 'null' is returned.
+# TODO: apply waiting tracking id build
 #
 # Input:
 #   PROJECT_NAME - name of project to start build for
+#   NAMESPACE - name of space: bsc-test, bsc-prod ....
 #
 # Output:
 #   build number
 ##
 function trigger_build {
-    local PROJECT_NAME=$1
+    local NAMESPACE=$1
+    local PROJECT_NAME=$2
     require_not_null "Project name not speficied" ${PROJECT_NAME} 
     BRANCH=$(get_branch required)
     NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
     BODY="$(cat <<-EOM
     {
-        "event_type": "build-${PROJECT_NAME}",
+        "event_type": "build-${NAMESPACE}-${PROJECT_NAME}",
         "client_payload": {
-            "job": "$3",
-            "namespace": "${PROJECT_NAME}"
+            "job": "${PROJECT_NAME}",
+            "namespace": "${NAMESPACE}"
         }
     }
 EOM
     )"
+    echo $BODY
     post dispatches "${BODY}"
-    for (( WAIT_SECONDS=0; WAIT_SECONDS<=5; WAIT_SECONDS+=1 )); do
-        WFS=$(get 'actions/runs?event=repository_dispatch' | jq '[ .workflow_runs[] | select(.created_at > "'${NOW}'" and .head_branch == "'${BRANCH}'") ]')
-        ID='null'
-        for JOBS_URL in $(echo "$WFS" | jq -r 'map(.jobs_url) | .[]'); do 
-            JOBS_URL=${JOBS_URL/$GITHUB_URL/}
-            ID=$(get ${JOBS_URL:1} | jq '[ .jobs[] | select(.name == "'${PROJECT_NAME}'") ] | map(.run_id) | .[0]')
-            if [[ ${ID} != 'null' ]]; then 
-                break
-            fi
-        done
-        if [[ ${ID} == 'null' ]]; then 
-            sleep 1
-        else
-            echo ${ID}
-            return
-        fi
-    done
-    echo 'null'
-}
-
-##
-# Get status of github build
-#
-# Input:
-#   BUILD_ID - id of build (workflow run id)
-#
-# Output:
-#   success | failed | null
-##
-function get_build_status {
-    local BUILD_ID=$1
-    require_not_null "Build id not speficied" ${BUILD_ID} 
-    STATUS_RESPONSE=$(get actions/runs/${BUILD_ID})
-    STATUS=$(echo "$STATUS_RESPONSE" | jq -r .conclusion)
-    case $STATUS in
-        success)
-            echo "success"
-            ;;
-        failure|cancelled)
-            echo "failed"
-            ;;
-        skipped)
-            echo "skipped"
-            ;;
-        *)
-            echo "null"
-            ;;
-    esac
-}
-
-
-##
-# Kill github build
-#
-# Input:
-#   BUILD_ID - id of build (workflow run id)
-##
-function kill_build {
-    local BUILD_ID=$1
-    require_not_null "Build id not speficied" ${BUILD_ID} 
-    STATUS_RESPONSE=$(post actions/runs/${BUILD_ID}/cancel)
-}
-
-##
-# Get revision hash of last successful commit which invokes main monorepository build
-#
-# Output:
-#   revision hash or null when there were no commits yet
-##
-function get_last_successful_commit {
-    BRANCH=$(get_branch)
-    if [[ -n "$BRANCH" ]]; then
-        BRANCH_FILTER='and .head_branch == "'${BRANCH}'"'
-    fi
-    SELECTOR='.conclusion == "success" '${BRANCH_FILTER}''
-    get 'actions/workflows/main.yml/runs' | jq -r "[ .workflow_runs[] | select($SELECTOR) ] | max_by(.run_number | tonumber).head_sha"
-}
-
-##
-# Get revision hash of current commit
-#
-# Output:
-#   revision hash or null when there were no commits yet
-##
-function get_current_commit {
-    require_env_var GITHUB_SHA
-    echo "$GITHUB_SHA"
 }
 
 ##
@@ -258,25 +159,9 @@ case $1 in
     build)        
         trigger_build $2 $3
         ;;
-    status)
-        get_build_status $2
+    help)
+        log "$USAGE_TEXT"
         ;;
-    kill)
-        kill_build $2
-        ;;    
-    hash)
-        case $2 in
-            last)
-                get_last_successful_commit
-                ;;
-            current)
-                get_current_commit
-                ;;
-            *)
-                fail "Unknown hash position $2"             
-                ;;
-        esac
-        ;;        
     *)
         fail "Unknown command $1"
         ;;        
